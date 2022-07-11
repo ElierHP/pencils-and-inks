4a986c5960402b00f08d884fb54150af52390ac1
Elier H <elierporto@gmail.com>
2022-07-11 19:15:54 -0400

    Add react-use-form with Yup validations.

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,53 +1,46 @@
-import React, { useState } from "react";
 import Layout from "../components/layout/Layout";
 import axios from "axios";
 import { useRouter } from "next/router";
 import { BASE_URL } from "../utils/api";
+import { userSchema } from "../validations/user";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
   const router = useRouter();
 
-  const LoginUser = async (e) => {
-    e.preventDefault();
-    try {
-      // Send login request to server
-      const res = await axios.post(
-        `${BASE_URL}/login`,
-        {
-          email,
-          password,
-        },
-        { withCredentials: true }
-      );
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(userSchema),
+  });
 
-      // Redirect to home page if successful
-      if (res.status === 200) {
-        router.push("/");
-      }
-    } catch (error) {
-      console.log(error);
+  const LoginUser = async (data) => {
+    // Send login request to server
+    const res = await axios.post(`${BASE_URL}/login`, data, {
+      withCredentials: true,
+    });
+
+    // Redirect to home page if successful
+    if (res.status === 200) {
+      router.push("/");
     }
   };
+
+  const onSubmit = (data) => LoginUser(data);
 
   return (
     <Layout>
       <h1>Login</h1>
-      <form onSubmit={LoginUser}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <p>{errors.email?.message}</p>
         <label htmlFor="">Email: </label>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
+        <input type="email" {...register("email")} />
+        <p>{errors.password?.message}</p>
         <label htmlFor="">Password: </label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+        <input type="password" {...register("password")} />
         <input type="submit" />
       </form>
     </Layout>
---
+++ src/validations/user.js
@@ -0,0 +1,11 @@
+import * as yup from "yup";
+
+export const userSchema = yup
+  .object({
+    email: yup.string().email().required().min(6).max(255),
+    password: yup.string().required().min(6).max(255),
+    passwordConfirmation: yup
+      .string()
+      .oneOf([yup.ref("password"), null], "Passwords must match"),
+  })
+  .required();
