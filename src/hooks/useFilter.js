a577ad2b95359808b9aa10f72716964a4dcd5354
Elier H <elierporto@gmail.com>
2022-08-09 16:32:23 -0400

    Add price filters to the query.

--- src/hooks/useFilter.js
+++ src/hooks/useFilter.js
@@ -23,21 +23,35 @@ export default function useFilter(
       if (item.checked) tags += item.tag + ",";
     });
 
+    // If user changes price input, add this price query to the GET request.
+    let priceQuery = "";
+    if (minNum > 0 || maxNum < 100) {
+      priceQuery = `&price=${minNum},${maxNum}`;
+    }
+
     // If category is "all", search by tags only.
     if (category === "all") {
       tags === ""
-        ? setQuery(`/products?tags=${queryTag}`)
-        : setQuery(`/products?tags=${tags.slice(0, -1)}`);
+        ? setQuery(`/products?tags=${queryTag}${priceQuery}`)
+        : setQuery(`/products?tags=${tags.slice(0, -1)}${priceQuery}`);
     } else {
       // Else if there's a category, search by category.
       tags === ""
-        ? setQuery(`/products?category=${category}`)
-        : setQuery(`/products?category=${category}&tags=${tags.slice(0, -1)}`);
+        ? setQuery(`/products?category=${category}${priceQuery}`)
+        : setQuery(
+            `/products?category=${category}&tags=${tags.slice(
+              0,
+              -1
+            )}${priceQuery}`
+          );
     }
   };
 
   // Reset all the checkboxes.
-  const resetFilters = () =>
+  const resetFilters = () => {
     checkboxes.forEach((item) => item.setChecked(false));
+    setMinNum(0);
+    setMaxNum(100);
+  };
   return [minNum, setMinNum, maxNum, setMaxNum, handleSubmit, resetFilters];
 }
